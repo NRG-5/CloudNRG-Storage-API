f7d72f365990f7757cbad5ea32df9187f3e82eb9
neor1se <defneorise@gmail.com>
2025-05-15 14:31:42 -0500

    feat(iam): add constructor to aggregate

--- src/main/java/com/cloudnrg/api/iam/domain/model/aggregates/User.java
+++ src/main/java/com/cloudnrg/api/iam/domain/model/aggregates/User.java
@@ -1,5 +1,6 @@
 package com.cloudnrg.api.iam.domain.model.aggregates;
 
+import com.cloudnrg.api.iam.domain.model.events.UserCreationEvent;
 import com.cloudnrg.api.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
@@ -22,5 +23,15 @@ public class User extends AuditableAbstractAggregateRoot<User> {
 
     @NotNull
     String passwordHash;
+
+    public User(String username, String email, String passwordHash) {
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public void userCreation(){
+        this.registerEvent(new UserCreationEvent(this, this.getId()));
+    }
 
 }
