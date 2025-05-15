d7f069190a30932a4ae16bced7b10726358424a0
neor1se <defneorise@gmail.com>
2025-05-14 20:17:30 -0500

    chore: project initialization

---
+++ src/main/java/com/cloudnrg/api/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -0,0 +1,31 @@
+package com.cloudnrg.api.shared.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+import java.util.UUID;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Getter
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @Getter
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+}
---
+++ src/main/java/com/cloudnrg/api/shared/interfaces/rest/MessageResource.java
@@ -0,0 +1,4 @@
+package com.cloudnrg.api.shared.interfaces.rest;
+
+public record MessageResource(String message) {
+}
