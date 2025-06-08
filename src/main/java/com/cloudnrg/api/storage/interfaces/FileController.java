8a3574f7935de77487f9e670091a295e4cf3ed09
Favio Sebastián Landeo Simeón <faviomsg05@gmail.com>
2025-06-08 02:08:20 -0500

    feat: implemented new endpoints for file and folder controller.

--- src/main/java/com/cloudnrg/api/storage/interfaces/FileController.java
+++ src/main/java/com/cloudnrg/api/storage/interfaces/FileController.java
@@ -1,6 +1,9 @@
 package com.cloudnrg.api.storage.interfaces;
 
 import com.cloudnrg.api.storage.domain.model.commands.CreateFileCommand;
+import com.cloudnrg.api.storage.domain.model.commands.DeleteFileByIdCommand;
+import com.cloudnrg.api.storage.domain.model.commands.UpdateFileFolderCommand;
+import com.cloudnrg.api.storage.domain.model.commands.UpdateFileNameCommand;
 import com.cloudnrg.api.storage.domain.model.queries.GetFilesByFolderIdQuery;
 import com.cloudnrg.api.storage.domain.services.FileCommandService;
 import com.cloudnrg.api.storage.domain.services.FileQueryService;
@@ -91,6 +94,64 @@ public class FileController {
 
     }
 
+    @Operation(summary = "Update file name", description = "Updates the name of a file by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "File name updated successfully"),
+            @ApiResponse(responseCode = "404", description = "File not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+    })
+    @PutMapping(value = "/{fileId}/name", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<FileResource> updateFileName(@PathVariable UUID fileId, @RequestParam("fileName") String fileName) {
+        try {
+            var updateFileNameCommand = new UpdateFileNameCommand(fileId, fileName);
+            var updatedFile = fileCommandService.handle(updateFileNameCommand);
+            if (updatedFile.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            var fileResource = FileResourceFromEntityAssembler.toResourceFromEntity(updatedFile.get(), "ok");
+            return ResponseEntity.ok(fileResource);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new FileResource(null, "Error updating file name"));
+        }
+    }
 
+    @Operation(summary = "Update file folder", description = "Updates the folder of a file by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "File folder updated successfully"),
+            @ApiResponse(responseCode = "404", description = "File not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+    })
+    @PutMapping(value = "/{fileId}/folder", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<FileResource> updateFileFolder(@PathVariable UUID fileId, @RequestParam("folderId") UUID folderId) {
+        try {
+            var updateFileFolderCommand = new UpdateFileFolderCommand(fileId, folderId);
+            var updatedFile = fileCommandService.handle(updateFileFolderCommand);
+            if (updatedFile.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
+            var fileResource = FileResourceFromEntityAssembler.toResourceFromEntity(updatedFile.get(), "ok");
+            return ResponseEntity.ok(fileResource);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new FileResource(null, "Error updating file folder"));
+        }
+    }
+
+    @Operation(summary = "Delete file by ID", description = "Delete a file by its ID")
+    @DeleteMapping(value = "/{fileId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "204", description = "File deleted successfully"),
+            @ApiResponse( responseCode = "404", description = "File not found"),
+            @ApiResponse( responseCode = "401", description = "Unauthorized"),
+    })
+    public ResponseEntity<?> deleteFileById(@PathVariable UUID fileId) {
+        try {
+            fileCommandService.handle(new DeleteFileByIdCommand(fileId));
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
