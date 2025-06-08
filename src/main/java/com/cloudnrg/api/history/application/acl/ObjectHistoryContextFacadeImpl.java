cf6c204c9cbcd9e357eb2d3951ffbd8763bb26e1
Favio Sebastián Landeo Simeón <faviomsg05@gmail.com>
2025-06-08 13:58:43 -0500

    feat: implemented context facade for objects history.

--- src/main/java/com/cloudnrg/api/history/application/acl/ObjectHistoryContextFacadeImpl.java
+++ src/main/java/com/cloudnrg/api/history/application/acl/ObjectHistoryContextFacadeImpl.java
@@ -1,7 +1,13 @@
 package com.cloudnrg.api.history.application.acl;
 
 import com.cloudnrg.api.history.domain.model.aggregates.ObjectHistory;
+import com.cloudnrg.api.history.domain.model.commands.CreateObjectHistoryCommand;
+import com.cloudnrg.api.history.domain.model.commands.DeleteAllObjectsHistoryByFileIdCommand;
+import com.cloudnrg.api.history.domain.model.events.SaveObjectHistoryCreateFileEvent;
+import com.cloudnrg.api.history.domain.model.queries.GetAllObjectsHistoryByFileIdQuery;
 import com.cloudnrg.api.history.domain.model.queries.GetAllObjectsHistoryByUserIdQuery;
+import com.cloudnrg.api.history.domain.model.queries.GetLastObjectHistoryByFileIdQuery;
+import com.cloudnrg.api.history.domain.model.valueobjects.Action;
 import com.cloudnrg.api.history.domain.services.ObjectHistoryCommandService;
 import com.cloudnrg.api.history.domain.services.ObjectHistoryQueryService;
 import com.cloudnrg.api.history.interfaces.acl.ObjectHistoryContextFacade;
@@ -21,5 +27,39 @@ public class ObjectHistoryContextFacadeImpl implements ObjectHistoryContextFacad
     @Override
     public List<ObjectHistory> fetchAllObjectsHistoryByUserId(UUID userId) {
         return queryService.handle(new GetAllObjectsHistoryByUserIdQuery(userId));
+    }
+
+    @Override
+    public List<ObjectHistory> fetchAllObjectsHistoryByFileId(UUID fileId) {
+        return queryService.handle(new GetAllObjectsHistoryByFileIdQuery(fileId));
+    }
+
+    @Override
+    public Optional<ObjectHistory> fetchLastObjectHistoryByFileId(UUID fileId) {
+        return queryService.handle(new GetLastObjectHistoryByFileIdQuery(fileId));
+    }
+
+    @Override
+    public Optional<ObjectHistory> saveObjectHistoryCreateFile(UUID fileId, UUID userId, Action action) {
+        var command = new CreateObjectHistoryCommand(fileId, userId, action);
+        return commandService.handle(command);
+    }
+
+    @Override
+    public Optional<ObjectHistory> saveObjectHistoryUpdateFileName(UUID fileId, UUID userId, Action action) {
+        var command = new CreateObjectHistoryCommand(fileId, userId, action);
+        return commandService.handle(command);
+    }
+
+    @Override
+    public Optional<ObjectHistory> saveObjectHistoryUpdateFileParentFolder(UUID fileId, UUID userId, Action action) {
+        var command = new CreateObjectHistoryCommand(fileId, userId, action);
+        return commandService.handle(command);
+    }
+
+    @Override
+    public void deleteAllObjectsHistoryByFileId(UUID fileId) {
+        var command = new DeleteAllObjectsHistoryByFileIdCommand(fileId);
+        commandService.handle(command);
     }
 }
--- src/main/java/com/cloudnrg/api/history/interfaces/acl/ObjectHistoryContextFacade.java
+++ src/main/java/com/cloudnrg/api/history/interfaces/acl/ObjectHistoryContextFacade.java
@@ -1,10 +1,20 @@
 package com.cloudnrg.api.history.interfaces.acl;
 
 import com.cloudnrg.api.history.domain.model.aggregates.ObjectHistory;
+import com.cloudnrg.api.history.domain.model.valueobjects.Action;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ObjectHistoryContextFacade {
     List<ObjectHistory> fetchAllObjectsHistoryByUserId(UUID userId);
+    List<ObjectHistory> fetchAllObjectsHistoryByFileId(UUID fileId);
+    Optional<ObjectHistory> fetchLastObjectHistoryByFileId(UUID fileId);
+
+    Optional<ObjectHistory> saveObjectHistoryCreateFile(UUID fileId, UUID userId, Action action);
+    Optional<ObjectHistory> saveObjectHistoryUpdateFileName(UUID fileId, UUID userId, Action action);
+    Optional<ObjectHistory> saveObjectHistoryUpdateFileParentFolder(UUID fileId, UUID userId, Action action);
+
+    void deleteAllObjectsHistoryByFileId(UUID fileId);
 }
