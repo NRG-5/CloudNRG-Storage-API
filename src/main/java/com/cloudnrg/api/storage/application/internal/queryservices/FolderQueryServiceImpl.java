e48609cda2039adcd6b081eeaac69bf4ba8afcc1
Favio Sebastián Landeo Simeón <faviomsg05@gmail.com>
2025-06-19 14:53:59 -0500

    feat: implementation for the new query get hierarchy folder..

--- src/main/java/com/cloudnrg/api/storage/application/internal/queryservices/FolderQueryServiceImpl.java
+++ src/main/java/com/cloudnrg/api/storage/application/internal/queryservices/FolderQueryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cloudnrg.api.storage.application.internal.queryservices;
 
 import com.cloudnrg.api.storage.domain.model.aggregates.Folder;
+import com.cloudnrg.api.storage.domain.model.queries.GetFolderHierarchyQuery;
 import com.cloudnrg.api.storage.domain.model.queries.GetRootFolderByUserIdQuery;
 import com.cloudnrg.api.storage.domain.services.FolderQueryService;
 import com.cloudnrg.api.storage.infrastructure.persistence.jpa.repositories.FolderRepository;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -26,5 +30,19 @@ public class FolderQueryServiceImpl implements FolderQueryService {
     @Override
     public Optional<Folder> handle(GetRootFolderByUserIdQuery query) {
         return folderRepository.findFolderByUser_IdAndName(query.userId(), "root");
+    }
+
+    @Override
+    public Optional<List<Folder>> handle(GetFolderHierarchyQuery query) {
+        var folderOpt = folderRepository.findFolderById(query.folderId());
+        if (folderOpt.isEmpty()) return Optional.empty();
+        List<Folder> hierarchy = new ArrayList<>();
+        Folder current = folderOpt.get();
+        while (current != null) {
+            hierarchy.add(current);
+            current = current.getParentFolder();
+        }
+        Collections.reverse(hierarchy);
+        return Optional.of(hierarchy);
     }
 }
--- src/main/java/com/cloudnrg/api/storage/domain/services/FolderQueryService.java
+++ src/main/java/com/cloudnrg/api/storage/domain/services/FolderQueryService.java
@@ -1,10 +1,13 @@
 package com.cloudnrg.api.storage.domain.services;
 
 import com.cloudnrg.api.storage.domain.model.aggregates.Folder;
+import com.cloudnrg.api.storage.domain.model.queries.GetFolderHierarchyQuery;
 import com.cloudnrg.api.storage.domain.model.queries.GetRootFolderByUserIdQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FolderQueryService {
     Optional<Folder> handle(GetRootFolderByUserIdQuery query);
+    Optional<List<Folder>> handle(GetFolderHierarchyQuery query);
 }
