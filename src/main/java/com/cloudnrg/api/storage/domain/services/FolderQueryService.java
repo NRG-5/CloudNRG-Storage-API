515ce1b698e2011a81691ad40c53778c9665eb3f
NeoRise456 <defneorise@gmail.com>
2025-06-22 18:56:10 -0500

    feat: add GetFolderByIdQuery and endpoint to retrieve folder by ID

--- src/main/java/com/cloudnrg/api/storage/domain/services/FolderQueryService.java
+++ src/main/java/com/cloudnrg/api/storage/domain/services/FolderQueryService.java
@@ -2,9 +2,11 @@ package com.cloudnrg.api.storage.domain.services;
 
 import com.cloudnrg.api.storage.domain.model.aggregates.Folder;
 import com.cloudnrg.api.storage.domain.model.queries.GetFolderAscendantHierarchyQuery;
+import com.cloudnrg.api.storage.domain.model.queries.GetFolderByIdQuery;
 import com.cloudnrg.api.storage.domain.model.queries.GetFolderDescendantHierarchyQuery;
 import com.cloudnrg.api.storage.domain.model.queries.GetRootFolderByUserIdQuery;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +14,6 @@ public interface FolderQueryService {
     Optional<Folder> handle(GetRootFolderByUserIdQuery query);
     Optional<List<Folder>> handle(GetFolderAscendantHierarchyQuery query);
     Optional<Folder> handle(GetFolderDescendantHierarchyQuery query);
+
+    Optional<Folder> handle(GetFolderByIdQuery query);
 }
