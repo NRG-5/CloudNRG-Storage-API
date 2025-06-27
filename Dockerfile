67138786e82d315acf23fb64b89c8cea8612c117
NeoRise456 <defneorise@gmail.com>
2025-06-27 18:24:07 -0500

    feat: configure application properties and add Docker support for Redis and PostgreSQL

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM maven:3.9.9-eclipse-temurin-21-alpine as build
+COPY . .
+RUN mvn clean package -DskipTests
+
+FROM openjdk:21-jdk-slim
+COPY --from=build /target/api-0.0.1-SNAPSHOT.jar api-cloudnrg.jar
+EXPOSE 8090
+
+ENTRYPOINT ["java", "-jar", "api-cloudnrg.jar"]
--- src/main/java/com/cloudnrg/api/shared/infrastructure/cache/redis/configuration/RedisConfig.java
+++ src/main/java/com/cloudnrg/api/shared/infrastructure/cache/redis/configuration/RedisConfig.java
@@ -6,6 +6,7 @@ import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.grid.jcache.JCacheProxyManager;
 import org.redisson.config.Config;
 import org.redisson.jcache.configuration.RedissonConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -15,13 +16,14 @@ import javax.cache.Caching;
 
 @Configuration
 public class RedisConfig {
-
     @Bean
-    public Config config(){
+    public Config config(@Value("${spring.data.redis.host:localhost}") String host,
+                         @Value("${spring.data.redis.port:6379}") String port){
         Config config=new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.useSingleServer().setAddress(String.format("redis://%s:%s", host, port));
         return config;
     }
+
     @Bean
     public CacheManager cacheManager(Config config){
         CacheManager manager = Caching.getCachingProvider().getCacheManager();
